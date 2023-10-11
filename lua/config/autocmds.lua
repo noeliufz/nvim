ed420b24f4f2fbeddf96f40908052d8681a4e1ea
Fangzhou Liu <u7572718@anu.edu.au>
2023-10-11 20:20:23 +1100

    Change transparent bg

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,7 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.cmd("hi Normal ctermbg=None")
-vim.cmd("hi NonText ctermbg=None")
+vim.cmd("hi Normal ctermbg=None guibg=None")
+vim.cmd("hi NonText ctermbg=None guibg=None")
 vim.cmd("hi NeoTreeNormal ctermbg=None guibg=None")
 vim.cmd("hi NeoTreeNormalNC ctermbg=None guibg=None")
