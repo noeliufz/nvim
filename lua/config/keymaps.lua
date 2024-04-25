7fbd7f9983efe6ab2e811be7942837cfa5238012
Fangzhou Liu <noeliufz@gmail.com>
2024-04-25 13:23:49 +1000

    update: keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,8 @@
 -- Add any additional keymaps here
 -- 按下 jk 同时切换到普通模式
 vim.api.nvim_set_keymap("i", "jk", [[<Esc>]], { noremap = true, silent = true })
+-- Resize window using <ctrl> arrow keys
+vim.api.nvim_set_keymap("n", "<A-UP>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.api.nvim_set_keymap("n", "<A-DOWN>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.api.nvim_set_keymap("n", "<A-LEFT>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.api.nvim_set_keymap("n", "<A-RIGHT>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
